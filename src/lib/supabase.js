e33a99e06458a86e39fc5e6dcc81cce12dbe02ac
Fer Juarez <congcalafate@gmail.com>
2026-09-10 19:23:23 -0300

    preparar proyecto para deploy

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,8 +2,9 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import 'expo-sqlite/localStorage/install';
 
-const supabaseUrl = 'https://qansdfyosecfhzewtnvk.supabase.co';
-const supabasePublishableKey = 'sb_publishable_FixDGQL-y_2pcR-33XlJcg_8DE_5yqs';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabasePublishableKey =
+  process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
